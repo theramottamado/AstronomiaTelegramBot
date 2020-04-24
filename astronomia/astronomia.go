3577df2604ba5b7b8841e5b414fbc302512a1006
Tamado Sitohang <ramottamado@gmail.com>
2020-04-24 09:34:07 +0700

    add gcloud error handling

--- astronomia/astronomia.go
+++ astronomia/astronomia.go
@@ -1,12 +1,15 @@
 package astronomia
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/logging"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
+	mrpb "google.golang.org/genproto/googleapis/api/monitoredres"
 )
 
 type LinkedID struct {
@@ -14,39 +17,89 @@ type LinkedID struct {
 	GroupID int64
 }
 
-var unames map[LinkedID]bool
+var (
+	unames map[LinkedID]bool
+)
 
 func init() {
 	unames = map[LinkedID]bool{}
 }
 
 func AstronomiaBot(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	logClient, err := logging.NewClient(ctx, os.Getenv("GCLOUD_PROJECT"))
+	if err != nil {
+		log.Panic("[FATAL] Not a Gcloud project!")
+	}
+	defer logClient.Close()
+	if os.Getenv("FUNCTION_NAME") == "" {
+		log.Panic("[FATAL] Not a cloud function!")
+	}
+	logger := logClient.Logger(
+		"cloudfunctions.googleapis.com/cloud-functions",
+		logging.CommonResource(&mrpb.MonitoredResource{
+			Labels: map[string]string{
+				"function_name": os.Getenv("FUNCTION_NAME"),
+				"project_id":    os.Getenv("GCP_PROJECT"),
+				"region":        "us-central1",
+			},
+			Type: "cloud_function",
+		}),
+		logging.CommonLabels(map[string]string{
+			"execution_id": r.Header.Get("Function-Execution-Id"),
+		}),
+	)
+
 	token := os.Getenv("TOKEN")
 	webhookURL := os.Getenv("WEBHOOK_URL")
 	if webhookURL == "" {
-		log.Panic("[FATAL] No webhook URL specified!")
+		// log.Panic("[FATAL] No webhook URL specified!")
+		logger.Log(logging.Entry{
+			Severity: logging.Alert,
+			Payload:  "No webhook URL specified!",
+		})
 	}
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Panic("[FATAL] Token invalid.")
+		// log.Panic("[FATAL] Token invalid.")
+		logger.Log(logging.Entry{
+			Severity: logging.Alert,
+			Payload:  "Token invalid.",
+		})
 	}
 
 	bot.Debug = false
 
-	log.Printf("[INFO] Authorized on account: %s", bot.Self.UserName)
+	// log.Printf("[INFO] Authorized on account: %s", bot.Self.UserName)
+	logger.Log(logging.Entry{
+		Severity: logging.Debug,
+		Payload:  fmt.Sprintf("Authorized on account: %s", bot.Self.UserName),
+	})
 
 	_, err = bot.SetWebhook(tgbotapi.NewWebhook(webhookURL + "?" + bot.Token))
 	if err != nil {
-		log.Fatalf("[FATAL] Bot crashed. Stacktrace: %s", err)
+		// log.Fatalf("[FATAL] Bot crashed. Stacktrace: %s", err)
+		logger.Log(logging.Entry{
+			Severity: logging.Critical,
+			Payload:  fmt.Sprintf("Bot crashed. Stacktrace: %s", err),
+		})
 	}
 	_, err = bot.GetWebhookInfo()
 	if err != nil {
-		log.Fatalf("[FATAL] Bot crashed. Stacktrace: %s", err)
+		// log.Fatalf("[FATAL] Bot crashed. Stacktrace: %s", err)
+		logger.Log(logging.Entry{
+			Severity: logging.Critical,
+			Payload:  fmt.Sprintf("Bot crashed. Stacktrace: %s", err),
+		})
 	}
 	defer func() {
 		err := recover()
 		if err != nil {
-			log.Printf("[FATAL] Bot crashed. Stacktrace: %s", err)
+			// log.Printf("[FATAL] Bot crashed. Stacktrace: %s", err)
+			logger.Log(logging.Entry{
+				Severity: logging.Critical,
+				Payload:  fmt.Sprintf("Bot crashed. Stacktrace: %s", err),
+			})
 		}
 		w.WriteHeader(200)
 	}()
@@ -54,25 +107,41 @@ func AstronomiaBot(w http.ResponseWriter, r *http.Request) {
 	update := bot.HandleUpdate(w, r)
 
 	if update.Message.Text != "" {
-		log.Printf("[INFO] %s: %s", update.Message.From.UserName, update.Message.Text)
+		// log.Printf("[INFO] %s: %s", update.Message.From.UserName, update.Message.Text)
+		logger.Log(logging.Entry{
+			Severity: logging.Info,
+			Payload:  fmt.Sprintf("%s: %s", update.Message.From.UserName, update.Message.Text),
+		})
 	}
-	log.Printf("[DEBUG] Length of map is: %d", len(unames))
+	// log.Printf("[DEBUG] Length of map is: %d", len(unames))
 
 	if !update.Message.IsCommand() {
-		log.Printf("[DEBUG] User ID: %d", update.Message.From.ID)
-		log.Printf("[DEBUG] Chat ID: %d", update.Message.Chat.ID)
+		// log.Printf("[DEBUG] User ID: %d", update.Message.From.ID)
+		// log.Printf("[DEBUG] Chat ID: %d", update.Message.Chat.ID)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 		if _, ok := unames[LinkedID{UserID: update.Message.From.ID, GroupID: update.Message.Chat.ID}]; ok {
 			delete(unames, LinkedID{update.Message.From.ID, update.Message.Chat.ID})
 			msg.Text, err = GetWeather(update.Message.Chat.FirstName, update.Message.Chat.LastName, update.Message.Text)
 			if err != nil {
-				log.Printf("[ERROR] %s", err)
+				// log.Printf("[ERROR] %s", err)
+				logger.Log(logging.Entry{
+					Severity: logging.Error,
+					Payload:  fmt.Sprintf("Error: %s", err),
+				})
 				msg.Text = "It appears that " + fmt.Sprintf("error %s", err) + ", try another location!"
-				log.Printf("[INFO] %s: %s", bot.Self.UserName, msg.Text)
+				// log.Printf("[INFO] %s: %s", bot.Self.UserName, msg.Text)
+				logger.Log(logging.Entry{
+					Severity: logging.Info,
+					Payload:  fmt.Sprintf("%s: %s", bot.Self.UserName, msg.Text),
+				})
 				bot.Send(msg)
 				return
 			}
-			log.Printf("[INFO] %s: %s", bot.Self.UserName, msg.Text)
+			// log.Printf("[INFO] %s: %s", bot.Self.UserName, msg.Text)
+			logger.Log(logging.Entry{
+				Severity: logging.Info,
+				Payload:  fmt.Sprintf("%s: %s", bot.Self.UserName, msg.Text),
+			})
 			bot.Send(msg)
 		}
 	} else if update.Message.IsCommand() {
@@ -97,7 +166,11 @@ func AstronomiaBot(w http.ResponseWriter, r *http.Request) {
 		default:
 			msg.Text = "I don't know that command"
 		}
-		log.Printf("[INFO] %s: %s", bot.Self.UserName, msg.Text)
+		// log.Printf("[INFO] %s: %s", bot.Self.UserName, msg.Text)
+		logger.Log(logging.Entry{
+			Severity: logging.Info,
+			Payload:  fmt.Sprintf("%s: %s", bot.Self.UserName, msg.Text),
+		})
 		bot.Send(msg)
 	}
 	return
--- go.mod
+++ go.mod
@@ -2,6 +2,18 @@ module github.com/AstronomiaDev/AstronomiaTelegramBot
 
 go 1.13
 
-require github.com/go-telegram-bot-api/telegram-bot-api v1.0.1-0.20200319042609-5e339ed016b0
+require (
+	cloud.google.com/go v0.56.0 // indirect
+	cloud.google.com/go/logging v1.0.0
+	github.com/go-telegram-bot-api/telegram-bot-api v1.0.1-0.20200319042609-5e339ed016b0
+	github.com/golang/protobuf v1.4.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
+	google.golang.org/api v0.22.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/genproto v0.0.0-20200423170343-7949de9c1215
+	google.golang.org/grpc v1.29.1 // indirect
+)
 
 replace github.com/go-telegram-bot-api/telegram-bot-api => github.com/AstronomiaDev/telegram-bot-api v1.0.1-0.20200423011512-fb6cf3d993f8
